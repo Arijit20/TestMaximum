cbeae19d6a13d39c83457c7c5076b6de495f5e25
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-08 14:45:30 +0530

    [Arijit]Refactored : Used Generic Class

--- src/main/java/com/cg/TestMaximum.java
+++ src/main/java/com/cg/TestMaximum.java
@@ -1,6 +1,14 @@
 package com.cg;
 
-public class TestMaximum {
+public class TestMaximum <E extends Comparable<E>>{
+	
+	E a, b, c;
+	
+	public TestMaximum(E a, E b, E c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 	
 	public static <E extends Comparable<E>> E testMax(E a, E b, E c) {
 		E max = a;
