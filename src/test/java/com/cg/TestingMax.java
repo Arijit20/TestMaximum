e3814138a6b3a488da873a5f345c152e45a26916
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-08 12:14:35 +0530

    [Arijit]Added : Testing Max Number At 2nd Position In Test Case

--- src/test/java/com/cg/TestingMax.java
+++ src/test/java/com/cg/TestingMax.java
@@ -11,7 +11,12 @@ import org.junit.Assert;
 public class TestingMax {
    
 	@Test
-	public void testMaxiumNumber() {
+	public void testMaxiumNumberAtFirst() {
 		Assert.assertEquals((Integer)8, TestMaximum.testMax(8, 4, 7));	
 	}
+	@Test
+	public void testMaxiumNumberAtSecond() {
+		Assert.assertEquals((Integer)20, TestMaximum.testMax(4, 20, 7));	
+	}
 }
+
