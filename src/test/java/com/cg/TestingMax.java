c606c1f14910b874d187cef2ada3e90689bd1b6e
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-08 12:27:32 +0530

    [Arijit]Added : Max Float Among 3 Using Test Cases

--- src/test/java/com/cg/TestingMax.java
+++ src/test/java/com/cg/TestingMax.java
@@ -11,16 +11,29 @@ import org.junit.Assert;
 public class TestingMax {
    
 	@Test
-	public void testMaxiumNumberAtFirst() {
+	public void testMaximumIntegerAtFirst() {
 		Assert.assertEquals((Integer)8, TestMaximum.testMax(8, 4, 7));	
 	}
 	@Test
-	public void testMaxiumNumberAtSecond() {
+	public void testMaximumIntegerAtSecond() {
 		Assert.assertEquals((Integer)20, TestMaximum.testMax(4, 20, 7));	
 	}
 	@Test
-	public void testMaxiumNumberAtThird() {
+	public void testMaximumIntegerAtThird() {
 		Assert.assertEquals((Integer)45, TestMaximum.testMax(4, 20, 45));	
+	}
+	
+	@Test
+	public void testMaximumFloatAtFirst() {
+		Assert.assertEquals((Float)4.6f, TestMaximum.testMax(4.6f, 4.5f, 2.6f));	
+	}
+	@Test
+	public void testMaximumFloatAtSecond() {
+		Assert.assertEquals((Float)5.2f, TestMaximum.testMax(4.6f, 5.2f, 1.7f));	
+	}
+	@Test
+	public void testMaximumFloatrAtThird() {
+		Assert.assertEquals((Float)9.9f, TestMaximum.testMax(4.8f, 8.0f, 9.9f));	
 	}
 }
 
