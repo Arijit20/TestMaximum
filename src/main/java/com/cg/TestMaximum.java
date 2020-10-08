e3e4cb202eb8f79cdf10910393272ded3f3a2568
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-08 11:40:15 +0530

    [Arijit]Added : Initializing The Project

---
+++ src/main/java/com/cg/TestMaximum.java
@@ -0,0 +1,7 @@
+package com.cg;
+
+public class TestMaximum {
+    public static void main(String[] args) {
+    	System.out.println("Starting the Maxium Test Program.");
+    }
+}
---
+++ src/test/java/com/cg/TestingMax.java
@@ -0,0 +1,11 @@
+package com.cg;
+
+import org.junit.Test;
+
+import com.cg.TestMaximum;
+
+import static org.junit.Assert.*;
+
+public class TestingMax {
+   
+}
